6871b5709e5a852de0baf5ebb2a940c5157d0882
zhangmeng34 <zhangmeng34@baidu.com>
2023-10-12 10:39:11 +0800

    3 longest substring

---
+++ 3_longest_substring/main.cpp
@@ -0,0 +1,39 @@
+#include "../utils.hpp"
+
+int longest_substring(const std::string& str) {
+    int max_size = 0;
+    auto left = str.begin();
+    auto right = str.begin() + 1;
+    while (left != str.end()) {
+        while (right != str.end()) {
+            std::cout << "inspecting " << str.substr(left - str.begin(), right - left) << std::endl;
+            auto iter = std::find(left, right, *right);
+            if (iter < right - 1) {
+                max_size = std::max(max_size, int(right - left));
+                ++left;
+                ++right;
+                break;
+            }
+            if (iter == right - 1) {
+                max_size = std::max(max_size, int(right - left));
+                left = right;
+                ++right;
+                break;
+            }
+            if (iter > right - 1) {
+                ++right;
+                continue;
+            }
+        }
+        if (right == str.end()) {
+            break;
+        }
+    }
+    return max_size;
+}
+
+int main(int argc, char** argv) {
+    std::string str = "pwwkew";
+    std::cout << longest_substring(str) << std::endl;
+    return 0;
+}
