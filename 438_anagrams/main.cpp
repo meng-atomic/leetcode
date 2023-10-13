3704a04bae3e25552e88d99abfac9dfc9dac2cba
zhangmeng34 <zhangmeng34@baidu.com>
2023-10-13 10:31:25 +0800

    add 438 find anagrams

---
+++ 438_anagrams/main.cpp
@@ -0,0 +1,51 @@
+#include "../utils.hpp"
+
+// 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+
+// 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+
+ 
+
+// 示例 1:
+
+// 输入: s = "cbaebabacd", p = "abc"
+// 输出: [0,6]
+// 解释:
+// 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+// 起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+//  示例 2:
+
+// 输入: s = "abab", p = "ab"
+// 输出: [0,1,2]
+// 解释:
+// 起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+// 起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+// 起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+ 
+
+// 提示:
+
+// 1 <= s.length, p.length <= 3 * 104
+// s 和 p 仅包含小写字母
+
+std::vector<size_t> find_anagrams(const std::string& s, const std::string& p) {
+    std::vector<size_t> res;
+    size_t left = 0;
+    while (left < s.size() && (left + p.size()) <= s.size()) {
+        std::string str = s.substr(left, p.size());
+        std::sort(str.begin(), str.end());
+        if (str == p) {
+            res.push_back(left);
+        }
+        ++left;
+    }
+    return res;
+}
+
+int main(int argc, char** argv) {
+    std::string s = "bbbbbbbbbbb";
+    std::string p = "bb";
+    auto res = find_anagrams(s, p);
+    echo_container(res);
+    return 0;
+}
